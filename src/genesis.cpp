6bbbfcfc88617d7cbd6f13d4866c495d6cb99aca
Neil Booth <kyuupichan@gmail.com>
2017-06-05 11:51:32 +0900

    Update genesis.cpp.
    - CreateGenesisBlock moved to chainparams.{ccp,h}
    - Clean up includes

--- src/genesis.cpp
+++ src/genesis.cpp
@@ -2,71 +2,18 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "unlimited.h"
-#include "base58.h"
-#include "chain.h"
-#include "chainparams.h"
-#include "checkpoints.h"
-#include "clientversion.h"
-#include "consensus/consensus.h"
-#include "consensus/params.h"
-#include "consensus/validation.h"
-#include "consensus/merkle.h"
-#include "core_io.h"
-#include "expedited.h"
-#include "hash.h"
-#include "leakybucket.h"
-#include "main.h"
-#include "miner.h"
-#include "net.h"
-#include "parallel.h"
-#include "policy/policy.h"
-#include "primitives/block.h"
-#include "requestManager.h"
-#include "rpc/server.h"
-#include "stat.h"
-#include "thinblock.h"
-#include "timedata.h"
-#include "tinyformat.h"
-#include "tweak.h"
-#include "txmempool.h"
-#include "ui_interface.h"
-#include "util.h"
-#include "utilmoneystr.h"
-#include "utilstrencodings.h"
-#include "validationinterface.h"
-#include "version.h"
-
-#include <atomic>
-#include <boost/foreach.hpp>
+#include <stream>
+#include <string>
 #include <boost/lexical_cast.hpp>
-#include <boost/thread.hpp>
-#include <inttypes.h>
-#include <iomanip>
-#include <limits>
-#include <queue>
-#include <stdexcept>
 
-
-static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint32_t nNonce, uint32_t nBits, const CAmount& genesisReward)
-{
-    CMutableTransaction txNew;
-    txNew.nVersion = 1;
-    txNew.vin.resize(1);
-    txNew.vout.resize(1);
-    txNew.vin[0].scriptSig = CScript() << ((int) nBits) << CScriptNum(4) << std::vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
-    txNew.vout[0].nValue = genesisReward;
-    txNew.vout[0].scriptPubKey = genesisOutputScript;
-
-    CBlock genesis;
-    genesis.nTime    = nTime;
-    genesis.nBits    = nBits;
-    genesis.nNonce   = nNonce;
-    genesis.vtx.push_back(txNew);
-    genesis.hashPrevBlock.SetNull();
-    genesis.hashMerkleRoot = BlockMerkleRoot(genesis);
-    return genesis;
-}
+#include "arith_uint256.h"
+#include "base58.h"
+#include "chainparams.h"
+#include "pow.h"
+#include "rpc/server.h"
+#include "streams.h"
+#include "utilstrencodings.h"
+#include "consensus/validation.h"
 
 CBlock *proposedGenesisBlock=NULL;
 
@@ -85,17 +32,17 @@ extern UniValue genesis(const UniValue& params, bool fHelp)
   std::string genesisComment = params[1].getValStr();
   const CChainParams& chp = Params();
   const Consensus::Params& conp = chp.GetConsensus();
-  int genesisDiff = UintToArith256(conp.powLimit).GetCompact();  // default to easiest block supported by this chain
+  int genesisBits = UintToArith256(conp.powLimit).GetCompact();  // default to easiest block supported by this chain
   if (params.size() > 2)
   {
       std::string genesisDiffs = params[2].getValStr();
       if (genesisDiffs[1] == 'x' || genesisDiffs[1] == 'X')
       {
-          genesisDiff = std::stoul(genesisDiffs, nullptr, 16);
+          genesisBits = std::stoul(genesisDiffs, nullptr, 16);
       }
       else
       {
-          genesisDiff = boost::lexical_cast<int>(genesisDiffs);
+          genesisBits = boost::lexical_cast<int>(genesisDiffs);
       }
   }
 
@@ -106,7 +53,7 @@ extern UniValue genesis(const UniValue& params, bool fHelp)
   arith_uint256 bnTarget;
   bool fNegative, fOverflow;
 
-  bnTarget.SetCompact(genesisDiff, &fNegative, &fOverflow);
+  bnTarget.SetCompact(genesisBits, &fNegative, &fOverflow);
   if (fNegative || bnTarget == 0 || fOverflow)
   {
       throw JSONRPCError(RPC_INVALID_PARAMETER,"block nBits difficulty is invalid");
@@ -117,19 +64,24 @@ extern UniValue genesis(const UniValue& params, bool fHelp)
       throw JSONRPCError(RPC_INVALID_PARAMETER,"block nBits difficulty is greater than this chain allows");
   }
 
-
   CScript outputScript = GetScriptForDestination(payaddress.Get());
   CAmount genesisReward(5000000000);
-  CBlock block = CreateGenesisBlock(genesisComment.c_str(), outputScript, GetTime(), 0,   genesisDiff, genesisReward );
-
+  // BIP34 height prefix
+  CBlock block = CreateGenesisBlock(CScript() << 0, genesisComment, outputScript, GetTime(), 0,
+                                    genesisBits, 1, genesisReward);
   CBlock *pblock = &block;
   proposedGenesisBlock = pblock;
   uint256 hash = pblock->GetHash();
+
   while (!CheckProofOfWork(hash, pblock->nBits, conp))
   {
-      ++pblock->nNonce;
+      if (++pblock->nNonce == 0)
+          throw JSONRPCError(RPC_MISC_ERROR, "could not solve for a block");
+      if (pblock->nNonce % 10000000 == 0)
+          LogPrintf("Nonce: %u\n", pblock->nNonce);
       hash = pblock->GetHash();
   }
+
   proposedGenesisBlock = NULL;
 
   CValidationState state;
@@ -137,7 +89,7 @@ extern UniValue genesis(const UniValue& params, bool fHelp)
   std::ostringstream ret;
   std::string hexOutScript = HexStr(outputScript);
 
-  ret << "CreateGenesisBlock() parameters: Comment: " << genesisComment.c_str() << " OutputScript: ParseHex(\"" << hexOutScript.c_str() << "\") Time: " << pblock->nTime << " Nonce: " << pblock->nNonce << " nBits: " << pblock->nBits << " Version: " << pblock->nVersion << " Reward: " << genesisReward << "\n";
+  ret << "CreateGenesisBlock() parameters: Prefix: CScript() << 0 Comment: " << genesisComment << " OutputScript: ParseHex(\"" << hexOutScript << "\") Time: " << pblock->nTime << " Nonce: " << pblock->nNonce << " nBits: " << pblock->nBits << " Version: " << pblock->nVersion << " Reward: " << genesisReward << "\n";
 
   CDataStream stream(SER_NETWORK, PROTOCOL_VERSION);
   pblock->Serialize(stream, SER_NETWORK, PROTOCOL_VERSION);
